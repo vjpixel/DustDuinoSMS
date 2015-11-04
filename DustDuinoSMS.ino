e5bf59d80723fad8a91daccc941f67612e0f00ef
VJ pixel <pixel@memelab.com.br>
2015-11-04 16:53:54 -0200

    fix number to string conversion

--- DustDuinoSMS.ino
+++ DustDuinoSMS.ino
@@ -6,10 +6,13 @@
 //888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
 // CONFIGURE HERE
-unsigned long sampletime_ms = 10*60*1000; // = 10m * 60s * 1000ms | time interval for posting
+unsigned long sampletime_ms = 1*60*1000; // = 1m * 60s * 1000ms | time interval for posting
 char remoteNum[] = "000000000000"; // number of the FrontlineCloud phone
-  
+
 //888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+// Include library to convert float to string
+// #include<stdlib.h>
 
 // Include the GSM library
 #include <GSM.h>
@@ -33,7 +36,7 @@ unsigned long triggerOffP2;
 unsigned long pulseLengthP2;
 unsigned long durationP2;
 boolean valP2 = HIGH;
-boolean triggerP2 = false;Dust
+boolean triggerP2 = false;
 
 float ratioP1 = 0;
 float ratioP2 = 0;
@@ -74,18 +77,18 @@ void setup(){
 
 void loop()
 {
-  
+
   digitalWrite(7, HIGH);
 
   valP1 = digitalRead(1); // reads PM 10
   valP2 = digitalRead(0); // reads PM 2.5
-  
+
   if(valP1 == LOW && triggerP1 == false)
   {
     triggerP1 = true;
     triggerOnP1 = micros();
   }
-  
+
   if (valP1 == HIGH && triggerP1 == true)
   {
     triggerOffP1 = micros();
@@ -93,13 +96,13 @@ void loop()
     durationP1 = durationP1 + pulseLengthP1;
     triggerP1 = false;
   }
-  
+
   if(valP2 == LOW && triggerP2 == false)
   {
     triggerP2 = true;
     triggerOnP2 = micros();
   }
-  
+
   if (valP2 == HIGH && triggerP2 == true)
   {
     triggerOffP2 = micros();
@@ -107,30 +110,30 @@ void loop()
     durationP2 = durationP2 + pulseLengthP2;
     triggerP2 = false;
   }
-  
+
   // Function creates particle count and mass concentration
   // from PPD-42 low pulse occupancy (LPO).
   if ((millis() - starttime) > sampletime_ms)
   {
-    
+
     // Generates PM10 and PM2.5 count from LPO.
     // Derived from code created by Chris Nafis
     // http://www.howmuchsnow.com/arduino/airquality/grovedust/
-      
+
     ratioP1 = durationP1/(sampletime_ms*10.0);
     ratioP2 = durationP2/(sampletime_ms*10.0);
     countP1 = 1.1*pow(ratioP1,3)-3.8*pow(ratioP1,2)+520*ratioP1+0.62;
     countP2 = 1.1*pow(ratioP2,3)-3.8*pow(ratioP2,2)+520*ratioP2+0.62;
     float PM10count = countP2;
     float PM25count = countP1 - countP2;
-      
+
     // Assues density, shape, and size of dust
     // to estimate mass concentration from particle
     // count. This method was described in a 2009
     // paper by Uva, M., Falcone, R., McClellan, A.,
     // and Ostapowicz, E.
     // http://wireless.ece.drexel.edu/research/sd_air_quality.pdf
-      
+
     // begins PM10 mass concentration algorithm
     double r10 = 2.6*pow(10,-6);
     double pi = 3.14159;
@@ -139,23 +142,27 @@ void loop()
     double mass10 = density*vol10;
     double K = 3531.5;
     float concLarge = (PM10count)*K*mass10;
-      
+
     // next, PM2.5 mass concentration algorithm
     double r25 = 0.44*pow(10,-6);
     double vol25 = (4/3)*pi*pow(r25,3);
     double mass25 = density*vol25;
     float concSmall = (PM25count)*K*mass25;
 
-    String sPM10count = String(PM10count);
-    String sPM25count = String(PM25count);    
+    String sPM10count = String(PM10count); // dtostrf(FLOAT,WIDTH,PRECSISION,BUFFER);
+    //String sPM10count;
+    //dtostrf(PM10count,4,2,sPM10count);
+    String sPM25count = String(PM25count); // dtostrf(FLOAT,WIDTH,PRECSISION,BUFFER);
+    //String sPM25count;
+    //dtostrf(PM25count,4,2,sPM25count);
     String sConcLarge = String(concLarge);
     String sConcSmall = String(concSmall);
 
-    txtMsg = "PM10count "+sPM10count+" PM10 "+sConcLarge+" PM25count "+sPM25count+" PM25 "+sConcSmall;
+    txtMsg = "PM10count "+ sPM10count + " PM10 " + sConcLarge + " PM25count " + sPM25count + " PM25 " + sConcSmall;
     sms.beginSMS(remoteNum);
     sms.print(txtMsg);
     sms.endSMS();
-        
+
     durationP1 = 0;
     durationP2 = 0;
     starttime = millis();
@@ -164,4 +171,3 @@ void loop()
 }
 
 
-    
